b925eb0e5f457a36cfea1f48b08e1d2aa07840e8
poorlydefinedbehaviour <brunotj2015@hotmail.com>
2019-09-06 01:35:37 -0300

    :white_check_mark: adds utility to load routes

--- src/utils/MakeRoutes.ts
+++ src/utils/MakeRoutes.ts
@@ -3,7 +3,7 @@ import { join } from "path";
 
 import { Express } from "express-serve-static-core";
 
-export const make_Routes = (app: Express): void => {
+export const make_routes = (server: Express): void => {
   const path: string = join(__dirname, "..", "http", "routes");
 
   const file_names: Array<string> = readdirSync(path).map(
@@ -12,6 +12,6 @@ export const make_Routes = (app: Express): void => {
 
   for (const file of file_names) {
     const { router } = require(join(path, file));
-    app.use("/api/v1/", router);
+    server.use("/api/v1/", router);
   }
 };
